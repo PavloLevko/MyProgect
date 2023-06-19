7da56030ae8db34b464406dcf12697f92ed61552
”Pavlo <levkopavlo@gmail.com>
2023-06-19 23:59:02 +0300

    deleted FileRider class

--- src/UserMenu.java
+++ src/UserMenu.java
@@ -1,45 +1,38 @@
-import com.sun.source.util.SourcePositions;
-
-import java.io.File;
-import java.sql.Statement;
 import java.util.Scanner;
 
 public class UserMenu extends Main {
- public static final String HALLO = "Hallo! ";
+    public static final String HALLO = "Hallo! ";
     public static final String EMPTHYPATH = "Empthy path! Try again.";
     public static final String IMPUTPATH = "Please input path to file.";
+    public static final String CORRECTPATH = "Correct path to file";
     public static boolean isRaning;
     public static String PATH;
 
 
-
-
-
-    public static void sayHallo (){
+    public static void sayHallo() {
 
         System.out.println(HALLO);
     }
 
-    public static void scanPath (){
+    public static void scanPath() {
         Scanner scan = new Scanner(System.in);
 
-        while (isRaning = true){
-        System.out.println(IMPUTPATH);
-        PATH = scan.nextLine();
-
-        if (PATH.isEmpty()){
-
-            System.out.println(EMPTHYPATH);
-
-        }else if (){
-
+        while (isRaning = true) {
+            System.out.println(IMPUTPATH);
             PATH = scan.nextLine();
 
-            isRaning =false;
+            if (MyFileRider.file.exists()) {
+
+                System.out.println(CORRECTPATH);
+
+            } else if (PATH.isEmpty()) {
+                System.out.println(CORRECTPATH);
 
 
+                isRaning = false;
 
-        }
+
+            }
 
         }
 
