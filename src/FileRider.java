f6b2e0240c0bed2cf07f4261b94503c55aeacbb1
”Pavlo <levkopavlo@gmail.com>
2023-06-19 23:02:41 +0300

    new methods in Class UserMenu

---
+++ src/FileRider.java
@@ -0,0 +1,5 @@
+import java.io.File;
+
+public class FileRider {
+    File file = new File("test.txt");
+}
--- src/UserMenu.java
+++ src/UserMenu.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class UserMenu {
+import com.sun.source.util.SourcePositions;
+
+import java.io.File;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class UserMenu extends Main {
+ public static final String HALLO = "Hallo! ";
+    public static final String EMPTHYPATH = "Empthy path! Try again.";
+    public static final String IMPUTPATH = "Please input path to file.";
+    public static boolean isRaning;
+    public static String PATH;
+
+
+
+
+
+    public static void sayHallo (){
+
+        System.out.println(HALLO);
+    }
+
+    public static void scanPath (){
+        Scanner scan = new Scanner(System.in);
+
+        while (isRaning = true){
+        System.out.println(IMPUTPATH);
+        PATH = scan.nextLine();
+
+        if (PATH.isEmpty()){
+
+            System.out.println(EMPTHYPATH);
+
+        }else if (){
+
+            PATH = scan.nextLine();
+
+            isRaning =false;
+
+
+
+        }
+
+        }
+
+    }
+
 }
