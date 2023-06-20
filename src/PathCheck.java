6bc422f040860a0c35d6551481ed9af0fc26efec
”Pavlo <levkopavlo@gmail.com>
2023-06-20 23:33:04 +0300

    Changes made in classes:PathCheck, MyFileRider Edited method checkedPath

--- src/PathCheck.java
+++ src/PathCheck.java
@@ -1,18 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.nio.file.Path;
 
 public class PathCheck extends MyFileRider {
 
-    private static final String IS_EXISTS = "Correct path to file.";
+    private static final String IS_EXISTS = "Correct path to file. Start reading...";
     private static final String WRONG_PATH = "You input wrong path to file";
+    public static File file = new File("/Users/macbook/IdeaProjects/MyProject/src/test.txt");
 
 
-    public static void checkedPath (String path){
+    public static String checkedPath (String path) throws FileNotFoundException {
 
-       if (MyFileRider.file.exists()){
+       if (file.exists()){
            System.out.println(IS_EXISTS);
+           MyFileRider.readFile(file);
+
        }else{
            System.out.println(WRONG_PATH);
        }
-
+return path;
     }
 }
